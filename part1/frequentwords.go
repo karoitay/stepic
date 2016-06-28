b3fcc9c3f164bc725381949724398a2566fdad4c
karoitay <karoitay@gmail.com>
2016-06-28 19:17:39 +0300

    FrequentWordsWithMismatch and FrequentWordsWithMismatchAndReverseComplenent

--- part1/frequentwords.go
+++ part1/frequentwords.go
@@ -1,23 +1,100 @@
 package part1
 
-import "strings"
+var others = map[string][]string{
+	"A": []string{"C", "G", "T"},
+	"C": []string{"A", "G", "T"},
+	"G": []string{"A", "C", "T"},
+	"T": []string{"A", "C", "G"},
+}
 
-func FrequentWords(text string, k int) string {
+func FrequentWords(text string, k int) []string {
 	m := map[string]int{}
 	max := 0
+	res := []string{}
 	for i := 0; i <= len(text)-k; i++ {
 		pattern := text[i : i+k]
 		v := m[pattern] + 1
 		m[pattern] = v
 		if v > max {
 			max = v
+			res = []string{pattern}
+		} else if v == max {
+			res = append(res, pattern)
 		}
 	}
-	var res []string
-	for k, v := range m {
-		if v == max {
-			res = append(res, k)
+	return res
+}
+
+func Neighbours(pattern string, d int) map[string]int {
+	m := make(map[string]int)
+	if len(pattern) == 1 {
+		m[pattern] = 0
+		for _, n := range others[pattern] {
+			m[n] = 1
+		}
+		return m
+	}
+	firstChar := pattern[0:1]
+	for p, distance := range Neighbours(pattern[1:], d) {
+		m[firstChar+p] = distance
+		if distance < d {
+			for _, n := range others[firstChar] {
+				m[n+p] = distance + 1
+			}
 		}
 	}
-	return strings.Join(res, " ")
+	return m
+}
+
+func FrequentWordsWithMismatch(text string, k, d int) []string {
+	m := map[string]int{}
+	max := 0
+	res := []string{}
+	for i := 0; i <= len(text)-k; i++ {
+		for pattern := range Neighbours(text[i:i+k], d) {
+			v := m[pattern] + 1
+			m[pattern] = v
+			if v > max {
+				max = v
+				res = []string{pattern}
+			} else if v == max {
+				res = append(res, pattern)
+			}
+		}
+	}
+	return res
+}
+
+func FrequentWordsWithMismatchAndReverseComplenent(text string, k, d int) []string {
+	m := map[string]int{}
+	max := 0
+	resmap := map[string]bool{}
+	for i := 0; i <= len(text)-k; i++ {
+		for pattern := range Neighbours(text[i:i+k], d) {
+			rpattern := ReverseComplement(pattern)
+			v1, v2 := m[pattern]+1, m[rpattern]+1
+			m[pattern], m[rpattern] = v1, v2
+			if v1 > max {
+				max = v1
+				resmap = map[string]bool{pattern: true}
+			}
+			if v2 > max {
+				max = v2
+				resmap = map[string]bool{rpattern: true}
+			}
+			if v1 == max {
+				resmap[pattern] = true
+			}
+			if v2 == max {
+				resmap[rpattern] = true
+			}
+		}
+	}
+	res := make([]string, len(resmap), len(resmap))
+	i := 0
+	for k := range resmap {
+		res[i] = k
+		i++
+	}
+	return res
 }
--- part1/frequentwords_test.go
+++ part1/frequentwords_test.go
@@ -20,7 +20,77 @@ func TestFrequentWords(t *testing.T) {
 		{testinput{"CGGAGGACTCTAGGTAACGCTTATCAGGTCCATAGGACATTCA", 3}, "AGG"},
 	}
 	for _, pair := range tests {
-		s := strings.Split(FrequentWords(pair.input.text, pair.input.k), " ")
+		s := FrequentWords(pair.input.text, pair.input.k)
+		sort.Strings(s)
+		v := strings.Join(s, " ")
+		if v != pair.output {
+			t.Error("For", pair.input, "expected", pair.output, "got", v)
+		}
+	}
+}
+
+func TestNeighbours(t *testing.T) {
+	type testinput struct {
+		text string
+		d    int
+	}
+	type testpair struct {
+		input  testinput
+		output string
+	}
+	tests := []testpair{
+		{testinput{"ACG", 1}, "AAG ACA ACC ACG ACT AGG ATG CCG GCG TCG"},
+	}
+	for _, pair := range tests {
+		m := Neighbours(pair.input.text, pair.input.d)
+		var s []string
+		for k := range m {
+			s = append(s, k)
+		}
+		sort.Strings(s)
+		v := strings.Join(s, " ")
+		if v != pair.output {
+			t.Error("For", pair.input, "expected", pair.output, "got", v)
+		}
+	}
+}
+
+func TestFrequentWordsWithMismatch(t *testing.T) {
+	type testinput struct {
+		text string
+		k, d int
+	}
+	type testpair struct {
+		input  testinput
+		output string
+	}
+	tests := []testpair{
+		{testinput{"ACGTTGCATGTCGCATGATGCATGAGAGCT", 4, 1}, "ATGC ATGT GATG"},
+	}
+	for _, pair := range tests {
+		s := FrequentWordsWithMismatch(pair.input.text, pair.input.k, pair.input.d)
+		sort.Strings(s)
+		v := strings.Join(s, " ")
+		if v != pair.output {
+			t.Error("For", pair.input, "expected", pair.output, "got", v)
+		}
+	}
+}
+
+func TestFrequentWordsWithMismatchAndReverseComplenent(t *testing.T) {
+	type testinput struct {
+		text string
+		k, d int
+	}
+	type testpair struct {
+		input  testinput
+		output string
+	}
+	tests := []testpair{
+		{testinput{"ACGTTGCATGTCGCATGATGCATGAGAGCT", 4, 1}, "ACAT ATGT"},
+	}
+	for _, pair := range tests {
+		s := FrequentWordsWithMismatchAndReverseComplenent(pair.input.text, pair.input.k, pair.input.d)
 		sort.Strings(s)
 		v := strings.Join(s, " ")
 		if v != pair.output {
