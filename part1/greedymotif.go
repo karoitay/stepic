9af6aefc33ce3a60f6ae3f4955e5146ce333360c
karoitay <karoitay@gmail.com>
2016-07-03 20:11:00 +0300

    greedy motif search

---
+++ part1/greedymotif.go
@@ -0,0 +1,38 @@
+package part1
+
+func ProfileMostProbable(text string, k int, profile map[byte][]float64) string {
+	res := ""
+	max := -1.0
+	for i := 0; i <= len(text)-k; i++ {
+		prob := 1.0
+		for j := i; j < i+k; j++ {
+			prob *= profile[text[j]][j-i]
+		}
+		if prob > max {
+			max = prob
+			res = text[i : i+k]
+		}
+	}
+	return res
+}
+
+func GreedyMotifSearch(dna []string, k int) []string {
+	var bestMotifs []string
+	// The lower the score the better, so we initialize it with
+	// a value greater than the max possible score.
+	bestScore := k * len(dna)
+	for i := 0; i <= len(dna[0])-k; i++ {
+		motifs := []string{dna[0][i : i+k]}
+		for j := 1; j < len(dna); j++ {
+			profile := motifsProfile(motifs)
+			nextMotif := ProfileMostProbable(dna[j], k, profile)
+			motifs = append(motifs, nextMotif)
+		}
+		score := MotifsScore(motifs)
+		if score < bestScore {
+			bestMotifs = motifs
+			bestScore = score
+		}
+	}
+	return bestMotifs
+}
---
+++ part1/greedymotif_test.go
@@ -0,0 +1,58 @@
+package part1
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestProfileMostProbable(t *testing.T) {
+	type testinput struct {
+		text    string
+		k       int
+		profile map[byte][]float64
+	}
+	type testpair struct {
+		input  testinput
+		output string
+	}
+	tests := []testpair{
+		{
+			testinput{"ACCTGTTTATTGCCTAAGTTCCGAACAAACCCAATATAGCCCGAGGGCCT", 5, map[byte][]float64{
+				'A': []float64{0.2, 0.2, 0.3, 0.2, 0.3},
+				'C': []float64{0.4, 0.3, 0.1, 0.5, 0.1},
+				'G': []float64{0.3, 0.3, 0.5, 0.2, 0.4},
+				'T': []float64{0.1, 0.2, 0.1, 0.1, 0.2},
+			}},
+			"CCGAG",
+		},
+	}
+	for _, pair := range tests {
+		v := ProfileMostProbable(pair.input.text, pair.input.k, pair.input.profile)
+		if v != pair.output {
+			t.Error("For", pair.input, "expected", pair.output, "got", v)
+		}
+	}
+}
+
+func TestGreedyMotifSearch(t *testing.T) {
+	type testinput struct {
+		k   int
+		dna []string
+	}
+	type testpair struct {
+		input  testinput
+		output []string
+	}
+	tests := []testpair{
+		{
+			testinput{3, []string{"GGCGTTCAGGCA", "AAGAATCAGTCA", "CAAGGAGTTCGC", "CACGTCAATCAC", "CAATAATATTCG"}},
+			[]string{"CAG", "CAG", "CAA", "CAA", "CAA"},
+		},
+	}
+	for _, pair := range tests {
+		v := GreedyMotifSearch(pair.input.dna, pair.input.k)
+		if !reflect.DeepEqual(v, pair.output) {
+			t.Error("For", pair.input, "expected", pair.output, "got", v)
+		}
+	}
+}
